26c272fa8fd0b7bbe471af771ab091f5f7ba0880
kanaru0928 <kanaru0928@gmail.com>
2025-07-01 18:22:02 +0900

    chore: unmaximize when multiple start

--- server/src-tauri/src/lib.rs
+++ server/src-tauri/src/lib.rs
@@ -418,10 +418,11 @@ async fn api_root() -> impl IntoResponse {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_single_instance::init(|app, _, _| {
-            let _ = app
-                .get_webview_window("main")
-                .expect("Failed to get webview window")
-                .set_focus();
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.unminimize();
+                let _ = window.show();
+                let _ = window.set_focus();
+            }
         }))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
